e24c8050b5dd5e60fcb352cfe89712dd92b19de6
Oleg Aksenov <forgotten89@yandex.ru>
2018-03-04 23:25:26 +0300

    test_add_contact: refactoring

--- test_add_contact.py
+++ test_add_contact.py
@@ -34,45 +34,11 @@ class test_add_contact(unittest.TestCase):
         self.checkIfContactAdded(wd)
         self.logout(wd)
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
     def go_to_main_page(self, wd, byLink):
         if byLink:
             wd.find_element_by_link_text("home").click()
         else:
             wd.get("http://localhost/addressbook/")
-
-    def add_contact(self, wd, contact):
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.personal_info.first_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.personal_info.last_name)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.contact_info.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.contact_info.address)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.contact_info.phone)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.contact_info.email)
-
-        birthday = processBirthdayStr(contact.personal_info.birthday)
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[@name='bday']/option[text()='"+birthday['day']+"']").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[@name='bday']//option[text()='"+birthday['day']+"']").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[@name='bmonth']//option[text()='"+birthday['month']+"']").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[@name='bmonth']//option[text()='"+birthday['month']+"']").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(birthday['year'])
-        wd.find_element_by_xpath("//div[@id='content']/form/input[@value='Enter']").click()
 
     def login(self, wd, username, password):
         wd.find_element_by_id("content").click()
@@ -84,8 +50,53 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[@type='submit']").click()
 
+    def add_contact(self, wd, contact):
+        wd.find_element_by_link_text("add new").click()
+        self.fill_personal_info_fields(wd, contact.personal_info)
+        self.fill_contact_info_fields(wd, contact.contact_info)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[@value='Enter']").click()
+
+    def fill_personal_info_fields(self, wd, personal_info):
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(personal_info.first_name)
+
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(personal_info.last_name)
+
+        birthday = processBirthdayStr(personal_info.birthday)
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[@name='bday']/option[text()='" + birthday[
+            'day'] + "']").is_selected():
+            wd.find_element_by_xpath(
+                "//div[@id='content']/form/select[@name='bday']//option[text()='" + birthday['day'] + "']").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[@name='bmonth']//option[text()='" + birthday[
+            'month'] + "']").is_selected():
+            wd.find_element_by_xpath(
+                "//div[@id='content']/form/select[@name='bmonth']//option[text()='" + birthday['month'] + "']").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(birthday['year'])
+
+    def fill_contact_info_fields(self, wd, contact_info):
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact_info.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact_info.address)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact_info.phone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact_info.email)
+
     def checkIfContactAdded(self, wd):
         assert len(wd.find_elements_by_xpath("//table//tr[@name='entry']")) > 0
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
 
     def tearDown(self):
         self.wd.quit()
